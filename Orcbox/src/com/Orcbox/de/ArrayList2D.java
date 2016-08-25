d2dca925161aff60febdac1035e4fd72287070f8
eljoth <hick.lars@gmail.com>
2016-08-25 21:25:17 +0200

    Played with ArrayLists.

--- Orcbox/src/com/Orcbox/de/ArrayList2D.java
+++ Orcbox/src/com/Orcbox/de/ArrayList2D.java
@@ -13,16 +13,21 @@ public class ArrayList2D {
 	}
 
 	public void printList() {
-		for (String s : sList) {
-			System.out.println(s);
+		for (ArrayList<String> l : aList) {
+			for (String s : l) {
+				System.out.print(s + ",");
+			}
+			System.out.println();
 		}
 	}
-	
-	public void addToInnerArray(String title, String interpret, String album, String length){
+
+	public void addToInnerArray(String title, String interpret, String album, String length, String path) {
 		sList.add(title);
 		sList.add(interpret);
 		sList.add(album);
 		sList.add(length);
+		sList.add(path);
 		aList.add(sList);
+		sList = new ArrayList<String>();
 	}
 }
