37c289d1c4284dad1abbf968719e2f6959e22d88
Steezem <sawrin.michael@googlemail.com>
2016-08-25 21:43:12 +0200

    fenster visible gesetzt, JFrame klasse gelöscht

--- Orcbox/src/com/Orcbox/de/Interface.java
+++ Orcbox/src/com/Orcbox/de/Interface.java
@@ -19,10 +19,11 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JLabel;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JMenuItem;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 
-public class Interface {
-
-    private JFrame frame;
+public class Interface extends JFrame {
 
     /**
      * Launch the application.
@@ -36,10 +37,10 @@ public class Interface {
      * Initialize the contents of the frame.
      */
     private void initialize() {
-	frame = new JFrame("Orcbox");
-	frame.setBounds(100, 100, 450, 300);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.getContentPane().setLayout(null);
+	this.setBounds(100, 100, 450, 300);
+	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	this.getContentPane().setLayout(null);
+	this.setVisible(true);
 	
 	JButton btnPlay = new JButton("Play random song");
 	btnPlay.addActionListener(new ActionListener() {
@@ -47,11 +48,11 @@ public class Interface {
 		}
 	});
 	btnPlay.setBounds(10, 93, 118, 23);
-	frame.getContentPane().add(btnPlay);
+	this.getContentPane().add(btnPlay);
 	
 	JButton btnBurnCd = new JButton("Burn CD");
 	btnBurnCd.setBounds(331, 93, 93, 23);
-	frame.getContentPane().add(btnBurnCd);
+	this.getContentPane().add(btnBurnCd);
 	
 	JButton btnLibrary = new JButton("Open Library");
 	btnLibrary.addActionListener(new ActionListener() {
@@ -59,6 +60,29 @@ public class Interface {
 		}
 	});
 	btnLibrary.setBounds(10, 67, 118, 23);
-	frame.getContentPane().add(btnLibrary);
+	this.getContentPane().add(btnLibrary);
+	
+	JMenu mnPlay = new JMenu("Play");
+	mnPlay.setHorizontalAlignment(SwingConstants.CENTER);
+	mnPlay.setBounds(10, 170, 107, 22);
+	this.getContentPane().add(mnPlay);
+	
+	JMenuItem mntmPlayFirstSong = new JMenuItem("Play first Song in List");
+	mnPlay.add(mntmPlayFirstSong);
+	
+	JMenuItem mntmNewMenuItem = new JMenuItem("Play random Song");
+	mnPlay.add(mntmNewMenuItem);
+	
+	JMenuItem mntmNewMenuItem_1 = new JMenuItem("Select Song");
+	mnPlay.add(mntmNewMenuItem_1);
+	
+	JButton btnCurrent = new JButton("Show current Song");
+	btnCurrent.setBounds(10, 127, 131, 23);
+	btnCurrent.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+		
+	    }
+	});
+	this.getContentPane().add(btnCurrent);
     }
 }
